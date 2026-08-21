e622a4def639e1095ecec4112e726553b67135d7
ajaybabumeena23-web <ajaybabu.meena23@gmail.com>
2026-08-21 15:04:48 +0530

    Updated package-lock.json

--- src/app/stocks/page.tsx
+++ src/app/stocks/page.tsx
@@ -1,4 +1,4 @@
-﻿import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { TopicHubView } from "@/components/content/TopicHubView";
 import { getTopic } from "@/lib/content/topics";
